52be844c731026aa8df2d21343263d865aa69e76
Misha Kessler <misha.kessler@generalassemb.ly>
2020-03-03 16:05:32 -0500

    Update arrayDiffs.js

--- arrayDiffs.js
+++ arrayDiffs.js
@@ -1,5 +1,3 @@
-//ARRAY DIFFS
-
 const arrDifferences = (arr1, arr2) => {
 
 }
@@ -8,4 +6,4 @@ console.log(arrDifferences([1, 44, 2, 3, 5], [33, 1, 2, 3, 4, 5]))
 
 module.exports = {
   arrDifferences
-};
+}
