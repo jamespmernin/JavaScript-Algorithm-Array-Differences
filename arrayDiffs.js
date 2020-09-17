2108636080c25a0051d26f31533b743aa1d31eee
James Mernin <jamespmernin@gmail.com>
2020-09-17 17:47:00 -0400

    removed comments

--- arrayDiffs.js
+++ arrayDiffs.js
@@ -1,17 +1,7 @@
 //ARRAY DIFFERENCES
 
 const arrayDiffs = (arr1, arr2) => {
-  let uniqueNums = [];
-
-  arr1.forEach((num) => {
-    (arr2.includes(num)) ? null : uniqueNums.push(num);
-  })
-
-  arr2.forEach((num) => {
-    (arr1.includes(num)) ? null : uniqueNums.push(num);
-  })
-
-  return uniqueNums;
+  return arr1.filter((num) => !arr2.includes(num)).concat(arr2.filter((num) => !arr1.includes(num)));
 };
 
 console.log(arrayDiffs([1, 44, 2, 3, 5], [33, 1, 2, 3, 4, 5]));
