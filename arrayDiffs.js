d8f0270f956d84ae7a723a4aa02eb20fda44c99f
James Mernin <jamespmernin@gmail.com>
2020-09-17 16:56:02 -0400

    Initial algorithm finished, not optimized

--- arrayDiffs.js
+++ arrayDiffs.js
@@ -1,6 +1,19 @@
 //ARRAY DIFFERENCES
 
-const arrayDiffs = (arr1, arr2) => {};
+const arrayDiffs = (arr1, arr2) => {
+  let uniqueNums = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) {
+      uniqueNums.push(arr1[i]);
+    }
+  }
+  for (let j = 0; j < arr2.length; j++) {
+    if (!arr1.includes(arr2[j])) {
+      uniqueNums.push(arr2[j]);
+    }
+  }
+  return uniqueNums;
+};
 
 console.log(arrayDiffs([1, 44, 2, 3, 5], [33, 1, 2, 3, 4, 5]));
 
